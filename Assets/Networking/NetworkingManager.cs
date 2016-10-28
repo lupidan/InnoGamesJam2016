bdc059cb10fc432090f755228463372a6504fd6f
Dennis Bliefernicht <triphoenix@triphoenix.de>
2016-10-28 22:57:36 +0200

    Basic networking sample support

---
+++ Assets/Networking/NetworkingManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public delegate void GameStateReceiver(GameState gameState);
+
+public class NetworkingManager : MonoBehaviour
+{
+    private const int GamePort = 9874;
+    private const short MsgTypeGameStateC2S = 10000;
+    private const short MsgTypeGameStateS2C = 10001;
+
+    public event GameStateReceiver StateReceivers;
+
+    private NetworkClient _client;
+
+    public void StartHosting()
+    {
+        NetworkServer.Listen(GamePort);
+        NetworkServer.RegisterHandler(MsgTypeGameStateC2S, OnClientToServerStateMessage);
+        _client = ClientScene.ConnectLocalServer();
+        RegisterClientHandlers();
+    }
+
+    public void ConnectToHost(string hostname)
+    {
+        _client = new NetworkClient();
+        RegisterClientHandlers();
+        _client.Connect(hostname, GamePort);
+    }
+
+    private void RegisterClientHandlers()
+    {
+        _client.RegisterHandler(MsgType.Connect, OnConnected);
+        _client.RegisterHandler(MsgTypeGameStateS2C, OnServerToClientStateMessage);
+    }
+
+    public void OnConnected(NetworkMessage message)
+    {
+        Debug.Log("connected to server");
+    }
+
+    public void SendState(GameState gameState)
+    {
+        var message = new MessageGameState(gameState);
+        _client.Send(MsgTypeGameStateC2S, message);
+    }
+
+    private void OnServerToClientStateMessage(NetworkMessage networkMessage)
+    {
+        var message = networkMessage.ReadMessage<MessageGameState>();
+        if (StateReceivers != null)
+        {
+            StateReceivers(message.ToGameState());
+        }
+    }
+
+    private void OnClientToServerStateMessage(NetworkMessage networkMessage)
+    {
+        var message = networkMessage.ReadMessage<MessageGameState>();
+        var sendingClientId = networkMessage.conn.connectionId;
+
+        foreach (var connection in NetworkServer.connections)
+        {
+            var connectionId = connection.connectionId;
+            if (connectionId != sendingClientId)
+            {
+                NetworkServer.SendToClient(connectionId, MsgTypeGameStateS2C, message);
+            }
+        }
+    }
+}
---
+++ Assets/Networking/NetworkingManagerDebugUIThingy.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class NetworkingManagerDebugUIThingy : MonoBehaviour
+{
+    private bool _isAtStartup = true;
+    private NetworkingManager _networkingManager;
+
+    void Awake()
+    {
+        _networkingManager = GetComponent<NetworkingManager>();
+        Assert.IsNotNull(_networkingManager);
+
+        _networkingManager.StateReceivers += OnStateReceived;
+    }
+
+    void OnStateReceived(GameState gameState)
+    {
+        Debug.LogError("Received game state");
+    }
+
+    void Update()
+    {
+        if (_isAtStartup)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                _networkingManager.StartHosting();
+                _isAtStartup = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                _networkingManager.ConnectToHost("127.0.0.1");
+                _isAtStartup = false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                _networkingManager.SendState(new GameState());
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (_isAtStartup)
+        {
+            GUI.Label(new Rect(2, 10, 150, 100), "Press S for server");
+            GUI.Label(new Rect(2, 50, 150, 100), "Press C for client");
+        }
+        else
+        {
+            GUI.Label(new Rect(2, 10, 150, 100), "Press P to pend");
+        }
+    }
+
+    void OnGameState(GameState newGameState)
+    {
+        Debug.LogError("Such state, much wow!");
+
+    }
+}
