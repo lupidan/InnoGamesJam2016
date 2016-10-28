5ba9dd2d6746284817ed0525a73628540148ab62
Sergio R. Lumley <lumley256@gmail.com>
2016-10-29 01:44:51 +0200

    Created attack pattern definitions

---
+++ Assets/Model/Definitions/AttackPatternDefinition.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "Assets/AttackPattern", order = 1)]
+public class AttackPatternDefinition : ScriptableObject
+{
+    public int Width = 1;
+    public int Height = 1;
+    [SerializeField]
+    private int[] Data = new int[1];
+
+    public void SetData(uint x, uint y, int value)
+    {
+        if (x < Width && y < Height)
+        {
+            Data[Index(x,y)] = value;
+        }
+    }
+
+    public int GetData(uint x, uint y)
+    {
+        if (x < Width && y < Height)
+        {
+            return Data[Index(x,y)];
+        }
+        return 0;
+    }
+
+    public void Resize(int width, int height)
+    {
+        width = Mathf.Max(width, 1);
+        height = Mathf.Max(height, 1);
+
+        int[] newData = new int[width * height];
+        for (uint x = 0; x < Width && x < width; x++)
+        {
+            for (uint y = 0; y < Height && y < height; y++)
+            {
+                newData[x + y * width] = Data[Index(x, y)];
+            }
+        }
+        Width = width;
+        Height = height;
+        Data = newData;
+    }
+
+    public int GetMax()
+    {
+        return Mathf.Max(Data);
+    }
+
+    private int Index(uint x, uint y)
+    {
+        return (int)x + (int)y * Width;
+    }
+}
