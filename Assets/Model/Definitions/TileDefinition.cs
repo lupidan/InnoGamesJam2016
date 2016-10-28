e3db50a82a426af5e02a1c864e8ce0245e46514b
Daniel Lupiañez Casares <lupidan@gmail.com>
2016-10-28 21:36:21 +0200

    Adds tiles and units definitions

---
+++ Assets/Model/Definitions/TileDefinition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "Assets/Tile", order = 1)]
+public class TileDefinition : ScriptableObject {
+
+	public enum Type {
+		Grass,
+		Mountain,
+		Forest,
+        Swamp
+	}
+
+	public string identifier;
+	public Type type;
+	public int cost;
+	public int attackBonus;
+	public int defenseBonus;
+	
+}
---
+++ Assets/Model/Definitions/UnitDefinition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableObject", menuName = "Assets/Unit", order = 1)]
+public class UnitDefinition : ScriptableObject {
+
+	public enum Type {
+		Heavy,
+		Range,
+		Melee
+	}
+
+	public string identifier;
+	public string unitName;
+	public Type type;
+	public int maxHealth;
+	public int attack;
+	public int defense;
+}
