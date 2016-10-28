4823a5fa7d12b30bc0480d1d849d8b93e88b5b01
Carlo <carlo@Xyralitys-MacBook-Air.local>
2016-10-28 22:36:32 +0200

    Unit controller

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -1,22 +1,116 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Media;
 using UnityEngine.EventSystems;
 using UnityEngine.EventSystems;
 
-public class UnitController : MonoBehaviour, ISelectHandler {
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+public class UnitController : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator animator;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private AudioSource audioSource;
 
-    public void OnSelect(BaseEventData eventData)
+    public AudioClip clickedSound;
+
+    public AudioClip attackSound;
+
+    public AudioClip damagedSound;
+
+    public AudioClip dyingSound;
+
+    [SerializeField] public AudioClip movingSound;
+
+
+    public Vector2 selectedTarget;
+
+
+    // Use this for initialization
+    void Start()
     {
-        throw new System.NotImplementedException();
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool stoppedMoving;
+        if (stoppedMoving)
+        {
+            animator.
+        }
+    }
+
+    public void move()
+    {
+    }
+
+
+    private void showRange()
+    {
+        Debug.LogError("Not yet implemented");
+    }
+
+    private void showAttackRange()
+    {
+        Debug.LogError("Not yet implemented");
+    }
+
+    private void playClickedSound()
+    {
+        audioSource.clip = clickedSound;
+        audioSource.Play();
+    }
+
+    private void startPlayingMovingSound()
+    {
+        audioSource.clip = movingSound;
+        audioSource.Play();
+    }
+
+    private void stopPlayingMovingSound()
+    {
+        audioSource.Stop();
+    }
+
+    private void playAttackSound()
+    {
+        audioSource.clip = attackSound;
+        audioSource.Play();
+    }
+
+    private void playDamagedSound()
+    {
+        audioSource.clip = damagedSound;
+        audioSource.Play();
+    }
+
+    private void playDyingSound()
+    {
+        audioSource.clip = dyingSound;
+        audioSource.Play();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var selected = EventSystem.current.currentSelectedGameObject;
+        if (selected != gameObject)
+        {
+            EventSystem.current.SetSelectedGameObject(gameObject);
+        }
+
+        playClickedSound();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
     }
 }
