994caf5a79b0f515d28fcb989b9c9b5f07194ebb
Sergio R. Lumley <lumley256@gmail.com>
2016-10-29 15:17:20 +0200

    Removed Moving and Cheat movement patterns

--- Assets/Model/Definitions/UnitDefinition.cs
+++ Assets/Model/Definitions/UnitDefinition.cs
@@ -17,9 +17,9 @@ public class UnitDefinition : ScriptableObject
     public int maxHealth;
     public int attack;
     public int defense;
+    public int maxMovements;
+    public int maxCheatingMovements;
 
     public AttackPatternDefinition attackPattern;
-    public MovePatternDefinition movePattern;
-    public CheatMovePatternDefinition cheatMovePattern;
 
 }
