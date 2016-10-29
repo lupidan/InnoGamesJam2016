142b7d44d54e11b3a4d4166a826171dd67f184d5
Daniel Lupiañez Casares <lupidan@gmail.com>
2016-10-29 20:11:47 +0200

    Adds map camera scrolling

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MapController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class MapController : MonoBehaviour,
+	IBeginDragHandler,
+	IDragHandler,
+	IEndDragHandler {
+
+	private Vector3 initialCameraPosition = Vector3.zero;
 
 	public void OnBeginDrag(PointerEventData eventData) {
-		Debug.Log(eventData.worldPosition);
+		initialCameraPosition = Camera.main.transform.position;
 	}
 
 	public void OnDrag(PointerEventData eventData) {
-		Debug.Log(eventData.worldPosition);
+		Vector3 worldPressPosition = Camera.main.ScreenToWorldPoint(eventData.pressPosition);
+		Vector3 worldPosition = Camera.main.ScreenToWorldPoint(eventData.position);
+		Vector3 delta = worldPressPosition - worldPosition;
+		delta.z = 0.0f;
+		Vector3 cameraPosition = initialCameraPosition + delta;
+		Camera.main.transform.position = cameraPosition;
 	}
 
-	public void OnEndDrag(PointerEventData eventData)
-	{
-		Debug.Log(eventData.worldPosition);
-	}
+	public void OnEndDrag(PointerEventData eventData) {}
 
 }
