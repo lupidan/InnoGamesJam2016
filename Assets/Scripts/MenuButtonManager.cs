483067ce5c73a115878123abd909c661728166e1
Jan <jan@Jans-MacBook-Pro.local>
2016-10-29 18:11:02 +0200

    added game menu with new game functionality

---
+++ Assets/Scripts/MenuButtonManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour {
+
+	public void NewGame (string sceneName) {
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void ExitGame () {
+	
+	}
+}
