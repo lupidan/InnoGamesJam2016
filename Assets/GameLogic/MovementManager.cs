de97988897c4fdd8101f353b0541ae26d83b31d2
Jan <jannh@gmx.net>
2016-10-29 21:04:56 +0200

    added api for obtaining reachable positions

--- Assets/GameLogic/MovementManager.cs
+++ Assets/GameLogic/MovementManager.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MovementManager: MonoBehaviour
 {
 	public MovementManager ()
 	{
 	}
-		
+
+	public List<Position> getReachablePositions(List<TileController> allTiles, Unit unit) {
+		return new List<Position> ();
+	}
 }
 
