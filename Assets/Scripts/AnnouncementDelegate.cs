9d97345958e943630280546f8c99aaab5a96b2d7
Sergio R. Lumley <lumley256@gmail.com>
2016-10-30 07:24:49 +0100

    Added delegate for game status

---
+++ Assets/Scripts/AnnouncementDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AnnouncementDelegate : MonoBehaviour
+{
+
+    private ClientGameLogicManager clientGameLogicManager;
+    private Text textField;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    textField = gameObject.GetComponent<Text>();
+	    clientGameLogicManager = ClientGameLogicManager.GetClientLogicFromScene();
+	    clientGameLogicManager.StateUpdatedHandler += OnStateChanged;
+	}
+
+    public void OnStateChanged()
+    {
+        switch (clientGameLogicManager.CurrentVisibleState.CurrentPhase)
+        {
+            case GamePhase.WaitingForStart:
+                textField.text = "Waiting for player...";
+                break;
+            case GamePhase.Planning:
+                textField.text = "Choose you movement";
+                break;
+            case GamePhase.Revision:
+                textField.text = "Cheat time!";
+                break;
+            case GamePhase.Finished:
+                textField.text = "Player " + clientGameLogicManager.CurrentVisibleState.WinningPlayerId + " won!";
+                textField.fontSize = 24;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+    }
+
+}
