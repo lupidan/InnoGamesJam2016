c96e3ddb715832fc3461c5e9fb5389e344e87e76
Sergio R. Lumley <lumley256@gmail.com>
2016-10-30 06:42:42 +0100

    Added health bars

---
+++ Assets/Scripts/HealthDisplayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(UnitController))]
+public class HealthDisplayer : MonoBehaviour
+{
+    private UnitController unitController;
+    private TextMesh textMesh;
+    private int lastHealth = -1;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    unitController = GetComponent<UnitController>();
+	    textMesh = GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (lastHealth != unitController.unitData.healthPoints)
+	    {
+	        lastHealth = unitController.unitData.healthPoints;
+	        string value = Convert.ToString(unitController.unitData.healthPoints);
+	        textMesh.text = value;
+	    }
+
+	}
+}
