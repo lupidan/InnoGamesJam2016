24e78f43fa412e964b26680eafe7e7a9849fa3e3
Sergio R. Lumley <lumley256@gmail.com>
2016-10-30 05:22:11 +0100

    Added next turn button

---
+++ Assets/Scripts/NextTurnButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NextTurnButton : MonoBehaviour
+{
+    private ClientNetworkingManager clientNetworkingManager;
+
+    void Start()
+    {
+        var networkClientGameObject = GameObject.Find("NetworkClient");
+        clientNetworkingManager = networkClientGameObject.GetComponent<ClientNetworkingManager>();
+    }
+
+    public void GoToNextTurn()
+    {
+        // List<GameAction> gameActions = new List<GameAction>();
+        // clientNetworkingManager.SendActions();
+    }
+}
