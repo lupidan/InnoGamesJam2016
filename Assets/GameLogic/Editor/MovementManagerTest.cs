601661d56f41299f3d1acd1f4f0321e9d3fbcd84
Jan <jannh@gmx.net>
2016-10-30 02:04:43 +0200

    implemented movement cost calculation and added some unit tests

---
+++ Assets/GameLogic/Editor/MovementManagerTest.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class MovementManagerTest {
+
+	[Test]
+	public void EditorTest()
+	{
+		//Arrange
+		var gameObject = new GameObject();
+
+		//Act
+		//Try to rename the GameObject
+		var newGameObjectName = "My game object";
+		gameObject.name = newGameObjectName;
+
+		//Assert
+		//The object has a new name
+		Assert.AreEqual(newGameObjectName, gameObject.name);
+	}
+
+	[Test]
+	public void IsPositionValidTest() {
+		Debug.Log ("Testing is position valid");
+		var manager = new MovementManager ();
+		Assert.IsTrue (manager.isPositionValid (new Position (0, 0), 1, 1));
+		Assert.IsFalse (manager.isPositionValid (new Position (0, 1), 1, 1));
+		Assert.IsFalse (manager.isPositionValid (new Position (1, 0), 1, 1));
+		Assert.IsFalse (manager.isPositionValid (new Position (-1, 0), 1, 1));
+		Assert.IsFalse (manager.isPositionValid (new Position (0, -1), 1, 1));
+	}
+
+	[Test]
+	public void calculateRealCostsTest() {
+		var manager = new MovementManager ();
+		int[,] costs = {							{1,1,1,1,1},
+													{1,1,1,1,1},
+													{1,1,1,1,1},
+													{1,1,1,1,1},
+													{1,1,1,1,1}};
+
+		int[,] expectedRealCostsWithMaxCosts2 = {	{-1,-1,2,-1,-1},
+													{-1,2,1,2,-1},
+													{2,1,0,1,2},
+													{-1,2,1,2,-1},
+													{-1,-1,2,-1,-1}};		
+		
+		int[,] expectedRealCostsWithMaxCosts4 = {	{4,3,2,3,4},
+													{3,2,1,2,3},
+													{2,1,0,1,2},
+													{3,2,1,2,3},
+													{4,3,2,3,4}};		
+		Position startPosition = new Position(2,2);
+		int[,] realCostsWithMaxCost1 = manager.calculateRealCostsMatrix (costs, startPosition, 2);
+		Debug.Log ("Real costs;\n" + intMatrixToString(realCostsWithMaxCost1));
+		Assert.AreEqual (expectedRealCostsWithMaxCosts2, realCostsWithMaxCost1);
+		int[,] realCostsWithMaxCost2 = manager.calculateRealCostsMatrix (costs, startPosition, 4);
+		//Debug.Log ("Real costs;\n" + intMatrixToString(realCostsWithMaxCost2));
+		Assert.AreEqual (expectedRealCostsWithMaxCosts4, realCostsWithMaxCost2);
+	}
+
+	[Test]
+	public void calculateRealCostsTestWithObstacles() {
+		var manager = new MovementManager ();
+		int[,] costs = {							{1,1,1,1,1},
+													{1,0,0,0,1},
+													{1,1,1,0,1},
+													{0,0,0,0,1},
+													{1,1,1,1,1}};
+
+		int[,] expectedRealCosts = {				{4,5,6,7,8},
+													{3,-1,-1,-1,9},
+													{2,1,0,-1,10},
+													{-1,-1,-1,-1,11},
+													{16,15,14,13,12}};
+		Position startPosition = new Position(2,2);
+		int[,] realCosts = manager.calculateRealCostsMatrix (costs, startPosition, 0);
+		Debug.Log ("Real costs with obstacles;\n" + intMatrixToString(realCosts));	
+		Assert.AreEqual (expectedRealCosts, realCosts);
+	}
+
+	public string intMatrixToString(int[,] intMatrix) {
+		string result = "";
+		for (int x = 0; x < intMatrix.GetLength (0); x++) {
+			for (int y = 0; y < intMatrix.GetLength (1); y++) {
+				result += intMatrix [x, y];
+				result += " ";
+			}
+			result += "\n";
+		}
+		return result;
+	}
+}
--- Assets/GameLogic/MovementManager.cs
+++ Assets/GameLogic/MovementManager.cs
@@ -1,16 +1,143 @@
 ﻿using System;
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
-public class MovementManager: MonoBehaviour
+public class MovementManager
 {
 	public MovementManager ()
 	{
 	}
 
-	public List<Position> getReachablePositions(List<TileController> allTiles, Unit unit) {
-		return new List<Position> ();
+	public List<Position> getReachablePositions(int mapWidth, int mapHeight, List<TileController> allTiles, Unit unit) {
+		int[,] costsMatrix = transformToCostsMatrix (mapWidth, mapHeight, allTiles);
+		int[,] realCostsMatrix = calculateRealCostsMatrix (costsMatrix, unit.position, unit.Definition.maxMovements);
+		return toListOfPositions (realCostsMatrix, unit.Definition.maxMovements);
 	}
+	/// <summary>
+	/// Transforms the real costs matrix to a list positions. 
+	/// Returns positions whose real costs are smaller than the max costs, if max costs is greater than 0.
+	/// Returns all reachable positions regardless of costs, if maxCosts is 0 or less.
+	/// </summary>
+	/// <returns>The list of positions.</returns>
+	/// <param name="costMatrix">Cost matrix.</param>
+	/// <param name="maxCosts">Max costs. Will be ignored if 0 or less</param>
+	public List<Position> toListOfPositions(int[,] costMatrix, int maxCosts) {
+		List<Position> positions = new List<Position> ();
+		for (int x = 0; x < costMatrix.GetLength(0); x++) {
+			for (int y = 0; y < costMatrix.GetLength(1); y++) {
+				int costs = costMatrix[x,y];
+				if (costs > 0 && (maxCosts <= 0 || costs <= maxCosts)) {
+					positions.Add (new Position (x,y));
+				}
+			}
+		}
+		return positions;
+	}
+
+	/// <summary>
+	/// Calculates the real cost matrix relative to the startPosition.
+	/// Will only calculate real costs that are equal or smaller than maxCosts, if maxCosts is greater than 0.
+	/// Will calculate real costs of all reachable positions of maxCosts is 0 or less.
+	/// </summary>
+	/// <returns>The real cost matrix.</returns>
+	/// <param name="costMatrix">Cost matrix.</param>
+	/// <param name="startPosition">Start position.</param>
+	/// <param name="maxCosts">Max costs. Will be ignored if 0 or less</param>
+	public int[,] calculateRealCostsMatrix(int[,] costsMatrix, Position startPosition, int maxCosts) {
+		int width = costsMatrix.GetLength(0);
+		int height = costsMatrix.GetLength(1);
+		List<Position> activeTilesList = new List<Position>();
+		int[,] realCostsMatrix = new int[width,height];
+
+		//init
+		activeTilesList.Add(startPosition);
+		initializeIntMatrix (realCostsMatrix, -1);
+		realCostsMatrix [startPosition.x, startPosition.y] = 0;
+
+		while (activeTilesList.Count > 0) {
+			HashSet<Position> nextIterationActiveTiles = new HashSet<Position>();
+			//sort active tiles by cost ascending
+			List<Position> sortedActiveTilesList = activeTilesList.OrderBy(pos => realCostsMatrix[pos.x,pos.y]).ToList();
+			//loop over sorted tiles
+			foreach (Position activeTile in sortedActiveTilesList) {
+				int x = activeTile.x;
+				int y = activeTile.y;
+				int startCosts = realCostsMatrix [x,y];
+				//calculate adjacent real costs
+				List<Position> adjacentPositions = new List<Position>();
+				//adjacentPositions.Add(new Position(x - 1, y + 1)); // top left
+				adjacentPositions.Add(new Position(x, y + 1)); // top center
+				//adjacentPositions.Add(new Position(x + 1, y + 1)); // top right
+				adjacentPositions.Add(new Position(x - 1, y)); // left
+				adjacentPositions.Add(new Position(x + 1, y)); // right
+				//adjacentPositions.Add(new Position(x - 1, y - 1)); // bottom left
+				adjacentPositions.Add(new Position(x, y - 1)); // bottom center
+				//adjacentPositions.Add(new Position(x + 1, y - 1)); // bottom right
+				foreach (Position position in adjacentPositions) {
+					if (isPositionValid(position, width, height)) {
+						//process tile only if not already processed
+						if (realCostsMatrix[position.x,position.y] < 0) {
+							//some unreachable tiles will be processed multiple times but that should be ok, no need to overoptimize here
+							int costs = costsMatrix [position.x,position.y];
+							// check if tile is walkable
+							if (costs > 0) {
+								int realCosts = startCosts + costs;
+								//add tile to next active tiles only if movements left
+								if (maxCosts <= 0 || realCosts <= maxCosts) {
+									realCostsMatrix [position.x,position.y] = realCosts;
+									if (maxCosts <= 0 || realCosts < maxCosts) {
+										nextIterationActiveTiles.Add (position);
+									}
+								}								
+							}	
+						}
+					}
+				}				
+			}
+			//create list of all directly reachable tiles from active tiles
+			//mark reachable tiles active
+			//mark active tiles inactive
+			activeTilesList = nextIterationActiveTiles.ToList();
+		}
+		return realCostsMatrix;
+	}
+
+	public void initializeIntMatrix(int[,] intMatrix, int defaultValue) {
+		for (int x = 0; x < intMatrix.GetLength (0); x++) {
+			for (int y = 0; y < intMatrix.GetLength (1); y++) {
+				intMatrix [x, y] = defaultValue;
+			}
+		}
+	}
+
+	public bool isPositionValid(Position pos, int width, int height) {
+		return pos.x >= 0 && pos.x < width && pos.y >= 0 && pos.y < height;
+	}
+
+	/// <summary>
+	/// Transforms a list of TileController objects to a costs matrix of width and height.
+	/// Not walkable tiles will be assigned the cost '-1'.
+	/// Missing tiles will be assigned the cost '-1' (not walkable).
+	/// </summary>
+	/// <returns>The to costs matrix.</returns>
+	/// <param name="mapWidth">Map width.</param>
+	/// <param name="mapHeight">Map height.</param>
+	/// <param name="allTiles">All tiles.</param>
+	public int[,] transformToCostsMatrix(int mapWidth, int mapHeight, List<TileController> allTiles) {
+		int[,] costsMatrix = new int[mapWidth,mapHeight];
+		// ensure undefined tiles are not walkable
+		initializeIntMatrix (costsMatrix, -1);
+		foreach (TileController tile in allTiles) {
+			int value = tile.tileData.Definition.cost;
+			if (!tile.tileData.Definition.walkable) {
+				value = -1;
+			}
+			costsMatrix [tile.tileData.position.x,tile.tileData.position.y] = value;
+		}
+		return costsMatrix;
+	}
+
+
 }
 
